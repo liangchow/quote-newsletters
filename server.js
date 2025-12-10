7a9593c39760a67b573025d07235220efcd43bc9
liangchow <liangchern@gmail.com>
2025-12-10 15:40:38 +0800

    app.post signup

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ require('dotenv').config
 app.use(express.static('public'))
 
 // routes
+app.post('/signup', async (req, res) => {
 
+    // Create firebase record
+    const data = {
+        email: newEmail,
+    }
+
+    const db = await db.collection('users').doc(newEmail).set(data, {merge: true})
+
+})
 
 app.listen(PORT, () => console.log(`Server has started on port: ${PORT}`))
