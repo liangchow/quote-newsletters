bf5ef07b6898ca5aaa24cc304876e74746fef897
liangchow <liangchern@gmail.com>
2025-12-11 22:37:15 +0800

    Optimize server.js endpoint
    app.post(signup)

--- server.js
+++ server.js
@@ -10,15 +10,34 @@ app.use(express.static('public'))
 
 // Routes
 app.post('/signup', async (req, res) => {
+    // Safety clause
     const newEmail = (req.body && req.body.email || '').trim()
+
     if (!newEmail){
-        return res.status(400).send({ "message": 'Email required' })
+        return res.status(400).send({ message: 'Email required' })
     }
+
+    // Email verification, e.g., j.doe@example.com
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (!emailRegex.test(newEmail)){
+            return res.status(400).send({ message: 'Invalid email format' })
+        }
+
     try {
-        await db.collection('users').doc(newEmail).set({ email: newEmail }, { merge: true })
+        await db.collection('users').doc(newEmail).set({ 
+            email: newEmail,
+            subscribedAt: new Date().toDateString, 
+         }, { merge: true })
+
+         // Send success response
+         res.status(200).json({
+            message: "Sucessfully subscribed",
+            email: newEmail,
+         })
 
     } catch (err) {
-        res.status(500).send({ "message": 'Failed to register' })
+        console.log('Error: ', err)
+        res.status(500).send({ message: 'Failed to register' })
     }
 })
 
