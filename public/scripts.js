43a2ee1bae8299036b3c3bd1fde6ce8f60fe8f20
liangchow <liangchern@gmail.com>
2025-12-20 08:16:28 +0800

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -131,7 +131,7 @@ async function fetchRandomQuote(){
         }
 
         quoteTextOutput.textContent = `"${quote.text}"`
-        quoteAuthorOutput.textContent = `-${quote.author} || -Unknown`
+        quoteAuthorOutput.textContent = `-${quote.author || -Unknown}`
         quoteAreaOutput.textContent = `(${quote.area})`
 
     } catch(err){
