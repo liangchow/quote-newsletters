7c383a5417dbed905cbe0ff3e77d4542aeb1067c
liangchow <liangchern@gmail.com>
2025-12-22 16:55:01 +0800

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -19,15 +19,6 @@ const quoteForm = document.querySelector('.quoteSubmission')
 // Email verification criteria, e.g., j.doe@example.com
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS 
-    }
-})
-
-
 // Utility functions
 function trimInput(input){
     return input.trim().replace(/[<>]/g, '')
